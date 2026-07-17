26ae2a70bb6dfe6f79f4fed1243dd7910104c38d
Carolina Arias Jiménez <cariasj@ucenfotec.ac.cr>
2026-07-16 19:43:38 -0600

    test(limite): reemplazar @MockBean por @MockitoBean

--- src/test/java/com/piedpiper/carbonhub/limite/controller/LimiteEmisionesControllerTest.java
+++ src/test/java/com/piedpiper/carbonhub/limite/controller/LimiteEmisionesControllerTest.java
@@ -25,10 +25,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = LimiteEmisionesController.class)
@@ -48,13 +48,13 @@ class LimiteEmisionesControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @MockBean
+    @MockitoBean
     private EmpresaAutenticadaService empresaAutenticadaService;
 
-    @MockBean
+    @MockitoBean
     private LimiteEmisionesService service;
 
     @Test
