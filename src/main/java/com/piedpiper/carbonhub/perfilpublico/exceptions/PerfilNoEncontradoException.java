86f1f5d2fba3e151af9e54169fff97bb17855cbe
Alison Yeung <ayeungv@ucenfotec.ac.cr>
2026-07-29 17:33:16 -0600

    feat(perfil-publico): add DTOs and domain exception - task 1.1

---
+++ src/main/java/com/piedpiper/carbonhub/perfilpublico/exceptions/PerfilNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.piedpiper.carbonhub.perfilpublico.exceptions;
+
+public class PerfilNoEncontradoException extends RuntimeException {
+
+    public PerfilNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
